008142e42d3108e3a9c3155f3f894ed1851f79f9
OttJoh94 <olg.johansson@gmail.com>
2024-01-24 15:10:29 +0100

    setting upp model

---
+++ VeryInterestingApiAndAppBackend/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace VeryInterestingApiAndAppBackend.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Director { get; set; }
+        public int Length { get; set; }
+        public int MyProperty { get; set; }
+        public double Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? Category { get; set; }
+
+
+    }
+}
